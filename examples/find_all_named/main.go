482d5643833d743b0190691dda0ec52413b3491b
Alain Gilbert <alain.gilbert.15@gmail.com>
2022-07-01 23:26:10 -0700

    simplify example

--- examples/find_all_named/main.go
+++ examples/find_all_named/main.go
@@ -10,10 +10,8 @@ import (
 func main() {
 	world := mcq.NewWorld(os.Getenv("WORLD_PATH"))
 	mcq.Q(world).Find(func(result mcq.Result) {
-		if item, ok := result.Item.(mc.IItem); ok {
-			fmt.Printf("%s %v %s\n", result.Coord(), result, item.Display())
-		} else if entity, ok := result.Item.(mc.IEntity); ok {
-			fmt.Printf("%s %v %s\n", result.Coord(), result, entity.CustomName())
+		if i, ok := result.Item.(mc.INamed); ok {
+			fmt.Printf("%s %v %s\n", result.Coord(), result, i.CustomName())
 		}
 	}, mcq.WithCustomName(true))
 }
