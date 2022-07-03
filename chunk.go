faeffe4c344a91813e37e39505fda76f98337688
Alain Gilbert <alain.gilbert.15@gmail.com>
2022-07-03 14:53:10 -0700

    process chunk with only 1 palette element

--- chunk.go
+++ chunk.go
@@ -45,12 +45,25 @@ func (c *Chunk) SetData(data *nbt.NbtTree) {
 
 // Each iterates all blocks in a chunk
 func (c *Chunk) Each(clb func(blockID mc.ID, x, y, z int)) {
+	coordFromPos := func(section, blockPos int) (x int, y int, z int) {
+		y = blockPos / YDim
+		z = blockPos % YDim / XDim
+		x = blockPos % XDim
+		y += section * SectionHeight
+		y -= 64
+		return
+	}
 	sections := c.GetData().Root().Entries["sections"].(*nbt.TagNodeList)
 	for s := 0; s < NbSection; s++ {
 		section := sections.Get(s).(*nbt.TagNodeCompound)
 		blockStates := section.Entries["block_states"].(*nbt.TagNodeCompound)
 		palette := blockStates.Entries["palette"].(*nbt.TagNodeList)
 		if palette.Length() == 1 {
+			for blockPos := 0; blockPos < XDim*ZDim*SectionHeight; blockPos++ {
+				blockID := mc.ID(palette.Get(0).(*nbt.TagNodeCompound).Entries["Name"].(*nbt.TagNodeString).String())
+				x, y, z := coordFromPos(s, blockPos)
+				clb(blockID, x, y, z)
+			}
 			continue
 		}
 		data := blockStates.Entries["data"].(*nbt.TagNodeLongArray)
@@ -69,11 +82,7 @@ func (c *Chunk) Each(clb func(blockID mc.ID, x, y, z int)) {
 			indexRemaining := blockPos % (64 / ones)
 			blockPaletteIndex := int(uint8(lng>>(indexRemaining*ones)) & mask)
 			blockID := mc.ID(palette.Get(blockPaletteIndex).(*nbt.TagNodeCompound).Entries["Name"].(*nbt.TagNodeString).String())
-			y := blockPos / YDim
-			z := blockPos % YDim / XDim
-			x := blockPos % XDim
-			y += s * SectionHeight
-			y -= 64
+			x, y, z := coordFromPos(s, blockPos)
 			clb(blockID, x, y, z)
 		}
 	}
