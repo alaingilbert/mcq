85c6432afa9c41908e951e2ebe8831c0efa357f0
Alain Gilbert <alain.gilbert.15@gmail.com>
2022-07-05 02:20:23 -0700

    use getter methods

--- examples/brewingstand_specific_information/main.go
+++ examples/brewingstand_specific_information/main.go
@@ -32,12 +32,12 @@ func main() {
 				}
 				return "<empty>"
 			}
-			fmt.Printf("BrewingStand: Brew-Time: %d | Fuel: %d\n", s.BrewTime, s.Fuel)
-			fmt.Printf("Left potion slot:   %s\n", displaySlot(s.LeftPotionSlot))
-			fmt.Printf("Middle potion slot: %s\n", displaySlot(s.MiddlePotionSlot))
-			fmt.Printf("Right potion slot:  %s\n", displaySlot(s.RightPotionSlot))
-			fmt.Printf("Ingredient slot:    %s\n", displaySlot(s.IngredientSlot))
-			fmt.Printf("Fuel slot:          %s\n", displaySlot(s.FuelSlot))
+			fmt.Printf("BrewingStand: Brew-Time: %d | Fuel: %d\n", s.BrewTime(), s.Fuel())
+			fmt.Printf("Left potion slot:   %s\n", displaySlot(s.LeftPotionSlot()))
+			fmt.Printf("Middle potion slot: %s\n", displaySlot(s.MiddlePotionSlot()))
+			fmt.Printf("Right potion slot:  %s\n", displaySlot(s.RightPotionSlot()))
+			fmt.Printf("Ingredient slot:    %s\n", displaySlot(s.IngredientSlot()))
+			fmt.Printf("Fuel slot:          %s\n", displaySlot(s.FuelSlot()))
 		}
 		fmt.Println("----------------------------------------")
 	})
--- mc/brewing_stand.go
+++ mc/brewing_stand.go
@@ -6,35 +6,43 @@ import (
 
 type BrewingStand struct {
 	ContainerBlockEntity
-	BrewTime         int // The number of ticks the potions have to brew.
-	Fuel             int // Remaining fuel for the brewing stand. 20 when full, and counts down by 1 each time a potion is brewed.
-	LeftPotionSlot   IItem
-	MiddlePotionSlot IItem
-	RightPotionSlot  IItem
-	IngredientSlot   IItem
-	FuelSlot         IItem
+	brewTime         int // The number of ticks the potions have to brew.
+	fuel             int // Remaining fuel for the brewing stand. 20 when full, and counts down by 1 each time a potion is brewed.
+	leftPotionSlot   IItem
+	middlePotionSlot IItem
+	rightPotionSlot  IItem
+	ingredientSlot   IItem
+	fuelSlot         IItem
 }
+
+func (b BrewingStand) BrewTime() int           { return b.brewTime }
+func (b BrewingStand) Fuel() int               { return b.fuel }
+func (b BrewingStand) LeftPotionSlot() IItem   { return b.leftPotionSlot }
+func (b BrewingStand) MiddlePotionSlot() IItem { return b.middlePotionSlot }
+func (b BrewingStand) RightPotionSlot() IItem  { return b.rightPotionSlot }
+func (b BrewingStand) IngredientSlot() IItem   { return b.ingredientSlot }
+func (b BrewingStand) FuelSlot() IItem         { return b.fuelSlot }
 
 func BrewingStandFromNbt(node *nbt.TagNodeCompound) *BrewingStand {
 	brewingStand := new(BrewingStand)
 	brewingStand.blockEntity = *BlockEntityFromNbt(node)
-	brewingStand.BrewTime = node.Entries["BrewTime"].(*nbt.TagNodeShort).Int()
-	brewingStand.Fuel = node.Entries["Fuel"].(*nbt.TagNodeByte).Int()
+	brewingStand.brewTime = node.Entries["BrewTime"].(*nbt.TagNodeShort).Int()
+	brewingStand.fuel = node.Entries["Fuel"].(*nbt.TagNodeByte).Int()
 	brewingStand.items = make(BaseItems, 0)
 	if items, ok := node.Entries["Items"].(*nbt.TagNodeList); ok {
 		items.Each(func(node nbt.ITagNode) {
 			item := node.(*nbt.TagNodeCompound)
 			parsedItem := parseItemFromNbt(item)
 			if parsedItem.Slot() == 0 {
-				brewingStand.LeftPotionSlot = parsedItem
+				brewingStand.leftPotionSlot = parsedItem
 			} else if parsedItem.Slot() == 1 {
-				brewingStand.MiddlePotionSlot = parsedItem
+				brewingStand.middlePotionSlot = parsedItem
 			} else if parsedItem.Slot() == 2 {
-				brewingStand.RightPotionSlot = parsedItem
+				brewingStand.rightPotionSlot = parsedItem
 			} else if parsedItem.Slot() == 3 {
-				brewingStand.IngredientSlot = parsedItem
+				brewingStand.ingredientSlot = parsedItem
 			} else if parsedItem.Slot() == 4 {
-				brewingStand.FuelSlot = parsedItem
+				brewingStand.fuelSlot = parsedItem
 			}
 			brewingStand.items = append(brewingStand.items, parsedItem)
 		})
